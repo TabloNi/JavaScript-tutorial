353fb895541e5fcc405829ef7cedb9e7c9d47f99
TabloNi <tabloni0719@gmail.com>
2021-01-02 07:34:13 +0900

    Finish : 2.0

--- index.js
+++ index.js
@@ -29,6 +29,13 @@ const myInfo = {
 myInfo.gender = "sdfds"
 
 console.log(myInfo.myObj[1].kow)
+
+
+function sayHello(name,age){
+    console.log('hi',name,'age',age)
+}
+
+sayHello("nam", 20)
 // 자바 스크립트의 선언은 let으로 한다. var 은 변수 선언 const 는 상수 선언 이다.
 // 다 아는거긴 하네
 //배열은 c랑 똑같음. 근데 선언 타입이 없어서 여러 타입이 한번에 입력 가능함.
