6f0c611f2ead6b825eb17473ebe30d2977177678
TabloNi <tabloni0719@gmail.com>
2021-01-02 07:25:00 +0900

    Finish : 1.10

--- index.js
+++ index.js
@@ -5,6 +5,31 @@ const d = 20
 console.log(a-b)
 const daysOfWeek = ["mon", "Tue"];
 console.log(daysOfWeek[1]);
+
+
+const myInfo = {
+    name : "nam",
+    age : 33,
+    gender : "Male",
+    myArr : ["sfsdfds","dsfdsfd", "sdfsd"],
+
+    myObj :[{
+        hi : 3,
+        name : "efw",
+        kks : true
+    },
+    {
+
+        kow : 30,
+        nana : "asdas"
+    }
+]
+}
+
+myInfo.gender = "sdfds"
+
+console.log(myInfo.myObj[1].kow)
 // 자바 스크립트의 선언은 let으로 한다. var 은 변수 선언 const 는 상수 선언 이다.
 // 다 아는거긴 하네
 //배열은 c랑 똑같음. 근데 선언 타입이 없어서 여러 타입이 한번에 입력 가능함.
+//object 선언은 {}로 함. 선언 후에 값을 .접근을 통해 변경 가능.
