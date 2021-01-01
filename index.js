7eda0806dbc971d1905bc030a673dbf378b4fe75
TabloNi <tabloni0719@gmail.com>
2021-01-02 07:09:40 +0900

    Finish : 1.9

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ let b = 90
 var c = 60
 const d = 20
 console.log(a-b)
-
+const daysOfWeek = ["mon", "Tue"];
+console.log(daysOfWeek[1]);
 // 자바 스크립트의 선언은 let으로 한다. var 은 변수 선언 const 는 상수 선언 이다.
 // 다 아는거긴 하네
+//배열은 c랑 똑같음. 근데 선언 타입이 없어서 여러 타입이 한번에 입력 가능함.
