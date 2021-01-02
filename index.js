d00240c7b95361108f3089f11c85e5e312f82b61
TabloNi <tabloni0719@gmail.com>
2021-01-02 09:54:10 +0900

    Finish : 2.5

--- index.js
+++ index.js
@@ -77,6 +77,13 @@ function handles(){
 
 window.addEventListener("resize", handles)
 
+// const agde = prompt("age?")
+
+// if(agde > 20)
+// {
+//     console.log("holy")
+// }
+
 
 
 // 자바 스크립트의 선언은 let으로 한다. var 은 변수 선언 const 는 상수 선언 이다.
